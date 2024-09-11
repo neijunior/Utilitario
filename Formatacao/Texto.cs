cf53fddb1a427fc179f9c04313f226a642af9e6f
nei.faria <nei.faria@supplyi.com.br>
2024-09-11 15:29:39 -0300

    wipe

--- Formatacao/Texto.cs
+++ Formatacao/Texto.cs
@@ -2,32 +2,32 @@
 
 namespace Formatacao
 {
-    public static class Texto
+  public static class Texto
+  {
+    public static string Left(this string valor, int length)
     {
-        public static string Left(this string valor, int length)
-        {
-            if (valor != null)
-                return (valor.Length >= length ? valor.Substring(0, length) : valor);
-            else
-                return string.Empty;
-        }
-
-        public static string Right(this string valor, int length)
-        {
-            if (valor != null)
-                return (valor.Length >= length ? valor.Substring(valor.Length - length, length) : valor);
-            else
-                return string.Empty;
-        }
-
-        public static string SomenteNumeros(this string valor)
-        {
-            if (!string.IsNullOrEmpty(valor))
-            {
-                Regex rg = new Regex("[^0-9]");
-                return rg.Replace(valor, "").Trim();
-            }
-            return string.Empty;     /* se um número não for retornado, então pode retornar vazio */
-        }
+      if (valor != null)
+        return (valor.Length >= length ? valor.Substring(0, length) : valor);
+      else
+        return string.Empty;
     }
+
+    public static string Right(this string valor, int length)
+    {
+      if (valor != null)
+        return (valor.Length >= length ? valor.Substring(valor.Length - length, length) : valor);
+      else
+        return string.Empty;
+    }
+
+    public static string SomenteNumeros(this string valor)
+    {
+      if (!string.IsNullOrEmpty(valor))
+      {
+        Regex rg = new Regex("[^0-9]");
+        return rg.Replace(valor, "").Trim();
+      }
+      return string.Empty;     /* se um número não for retornado, então pode retornar vazio */
+    }
+  }
 }
