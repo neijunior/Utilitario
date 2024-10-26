f6524d2e4c1e1160139f4dc5790eb698abd81c30
NeiJunior <Nei Junior@DESKTOP-SS8QBJE>
2024-10-26 00:26:10 -0300

    wipe

---
+++ Formatacao/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formatacao
+{
+  public static class Converter
+  {
+    public static int ToInt(this string valor)
+    {
+      int ret = default(int);
+      if (!string.IsNullOrEmpty(valor))
+        ret = Convert.ToInt32(valor);
+      return ret;
+    }
+  }
+}
---
+++ Formatacao/Decimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formatacao
+{
+  public static class Decimal
+  {
+    public static decimal Truncar(this decimal valor, int qtdCasas = 2)
+    {
+      int valorCasas = ($"1{("").PadLeft(qtdCasas, '0')}").ToInt();
+      return Math.Truncate(valorCasas * valor) / 100;
+    }
+  }
+}
